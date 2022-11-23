9344aadef4205236d26c3f503aa6dabf9d70409c
destoer <destoer1234@protonmail.com>
2022-11-23 00:07:03 +0000

    add license

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 small library for personal projects - BSD 3 clause license
+
+usage:
+
+git submodule add https://github.com/destoer/destoer-lib
+
+include destoer-lib.cpp in include path
+
+#include <destoer-lib.cpp>
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,15 @@
 #include <destoer.cpp>
 
+using namespace destoer;
+
 int main()
 {
+    // TODO: expand this if need be, this is basically just a simple compile check atm
+    Array<String> buf;
 
+    String str = "Hey";
+
+    push_var(buf,str);
+
+    printf("'%s' : %d\n",buf[0].buf,buf[0] == "Hey");
 }
